27b1d159b16c630450b399b056de49be88305c59
ShurayukiHime <jos.7-11@hotmail.it>
2016-10-12 10:55:00 +0200

    Aggiunte chiavi private e pubbliche al myDataUser; da sistemare la generazione delle chiavi forse in altra classe; rimane in dubbio la psw

--- Tesi2.0/src/model/MyDataUser.java
+++ Tesi2.0/src/model/MyDataUser.java
@@ -1,28 +1,66 @@
 package model;
 
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * This class describes a generic MyData User. This kind of description does not
+ * depend on the particular service chosen, so it should be implemented as a
+ * starting base for the account the end-user creates at the service.
+ */
+
 public class MyDataUser {
+	private final String keyAlgorithm = "DSA";
+	private final String randomAlgorithm = "SHA1PRNG";
 	private String firstName;
 	private String lastName;
 	private Date dateOfBirth;
 	private String emailAddress;
-	//private JWK key;
-	//solo getter
+	// penso di aver bisogno di una password da qualche parte, ad esempio
+	// metti che l'utente vuole cambiare chiavi, bisogna chiedere conferma
+	// o in generale quando bisogna dare un consent (magari non tutte le volte?)
+	// però è giusto che stia qui dentro?
+	// forse è meglio fare una classe nascosta che contiene le chiavi e si occupa di generazione delle stesse,
+	// verifica, firma and so on..?
+	private KeyPair keyPair;
 	private List<Consent> consents;
+
+	private KeyPair generateKeys(String keyAlgorithm, String randomAlgorithm) throws NoSuchAlgorithmException {
+		//era meglio cablarli nel codice? (non so quanto sia utile metterli nel costruttore)
+		KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(keyAlgorithm);
+		SecureRandom random = SecureRandom.getInstance(randomAlgorithm);
+		keyPairGen.initialize(1024, random);
+		return keyPairGen.generateKeyPair();
+	}
 	
 	public MyDataUser() {
 		super();
 	}
 
+	/**
+	 * This method creates a new MyData user.
+	 * It also initialises an empty collection of Consent(s), and creates a new KeyPair for the user.
+	 */
 	public MyDataUser(String firstName, String lastName, Date dateOfBirth, String emailAddress) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.dateOfBirth = dateOfBirth;
 		this.emailAddress = emailAddress;
+		try {
+			this.keyPair = generateKeys(keyAlgorithm, randomAlgorithm);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		this.consents = new ArrayList<Consent>();
 	}
 
@@ -68,5 +106,13 @@ public class MyDataUser {
 		}
 	}
 	
+	public PublicKey getPublicKey() {
+		return keyPair.getPublic();
+	}
 	
+	public Signature signWithPrivateKey() throws InvalidKeyException, NoSuchAlgorithmException {
+		Signature signature = Signature.getInstance(keyAlgorithm);
+		signature.initSign(keyPair.getPrivate());
+		return signature;
+	}
 }
